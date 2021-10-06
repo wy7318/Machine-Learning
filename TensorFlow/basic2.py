8d2a30d11c113b6f0fa6ec84aa2994ba71c3026f
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-06 08:59:13 -0700

    Update basic2.py
    Linear Model creation, training evaluating, predicting added

--- TensorFlow/basic2.py
+++ TensorFlow/basic2.py
@@ -42,3 +42,44 @@ for feature_name in Numeric_Columns:
     feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
 print(feature_columns)
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df)) #create tf.data.Dataset object with data and its label
+        if shuffle:
+            ds = ds.shuffle(1000) #randomizing order of data
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds
+    return input_function
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+linear_est.train(train_input_fn) #Train
+result = linear_est.evaluate(eval_input_fn) #Get model metrics/stats by testing on testing data
+
+print(result['accuracy']) #result variable is simly a dict of stats about model
+print(result)
+
+result = list(linear_est.predict(eval_input_fn)) #Making list of predictions with configured linear model
+print('Information of person #3\n', dfeval.loc[2]) #printing third person's information in the dataset
+print('Probability of person #3 survival based on our linear model : ',result[2]['probabilities'][1]) #result of our third training input ([3]), check probabilities(['probabilities']), probability of not survival ([0] or survival as [1])
+print('Survival of person #3 (1: Survived, 0 : Not Survived):',y_eval.loc[3]) #Actual fact whether this person is survived or not
+
+'''Result
+Information of person #3
+ sex                        female
+age                            58
+n_siblings_spouses              0
+parch                           0
+fare                        26.55
+class                       First
+deck                            C
+embark_town           Southampton
+alone                           y
+Name: 2, dtype: object
+Probability of person #3 survival based on our linear model :  0.85323733
+Survival of person #3 (1: Survived, 0 : Not Survived): 1
+'''
