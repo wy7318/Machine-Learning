15bf15b767eb57c04b74c9a809d09c033fc491ad
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-07 16:18:59 -0700

    Update cluster.py

--- TensorFlow/cluster.py
+++ TensorFlow/cluster.py
@@ -14,10 +14,4 @@ K-Means Algorithm:
 4) Repeat the action to make K centroid being in the middle 
 5) When new K point exist, compare the distances between all K centroid and assign it to the most closest one
 
-Hidden Markov Models
-Hidden Markov Model is a statistical Markov model in which the system being modeled is assumed to be a Markov process – call it X – with unobservable states
-
-States : i.e. warm, cold, high, low
-Observations : i.e. "On a hot day, Matt has a 80% chance of being happy and a 20% change of being sad"
-Transitions : i.e. " A cold day has a 30% chance of being followed by a hot day and a 70% chance of being followed by another cold day"
 '''
