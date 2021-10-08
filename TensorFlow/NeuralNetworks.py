b694bca603285c0c253e3ae4645db8b20589636c
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-07 17:03:00 -0700

    Create NeuralNetworks.py

---
+++ TensorFlow/NeuralNetworks.py
@@ -0,0 +1,10 @@
+'''Neural Networks
+Densely Connected Neural Network
+Every node in different layers(input, hidden, output) is connected to each other.
+
+Connection : weight
+Bias : Some constant numeric value that's connected to the next layer. When bias is connected to another layer, weight is typically 1.
+
+layer's node value = [sum of (Previous connected Node Value)*(weight)] + (Bias value)*(Weight =1)
+
+'''
