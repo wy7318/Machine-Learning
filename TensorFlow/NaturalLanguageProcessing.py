be7282295e8a85e9bc6309c1757df7ba0a2988cb
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-14 14:57:10 -0700

    Update NaturalLanguageProcessing.py

--- TensorFlow/NaturalLanguageProcessing.py
+++ TensorFlow/NaturalLanguageProcessing.py
@@ -67,3 +67,68 @@ Word Embeddings
 
 Recurrent Neural
 '''
+'''
+Word Embeddings
+- Classify or translate every word into vector
+
+Recurrent Neural
+'''
+
+'''Sentiment Analysis
+- Movie Review Dataset
+'''
+
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+import tensorflow as tf
+import os
+import numpy as np
+
+VOCAB_SIZE = 88584
+
+MAXLEN = 250
+BATCH_SIZE = 64
+
+(train_data, train_label), (test_data, test_labels) = imdb.load_data(num_words = VOCAB_SIZE)
+
+train_data[0]   #Looking at one review
+
+#More Preprocessing
+'''
+Since each review has different length, we need to unified the length of review to pass into neural network
+If review > 250 words, trim off extra
+If review < 250 words, add the necessary amount of 0's to make it 250 words
+'''
+train_data = sequence.pad_sequences(train_data, MAXLEN)     #Perform matching length of the review for train data
+test_data = sequence.pad_sequences(test_data, MAXLEN)       #Perform matching length of the review for test data
+
+#Creating the model
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(VOCAB_SIZE, 32),
+    tf.keras.layers.LSTM(32),
+    tf.keras.layers.Dense(1, activation = "sigmoid")    #"sigmoid" sequeeze our value between 0 and 1. Like <0.5 review becomes bad and opposite for good review
+])
+
+model.summary()
+'''
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #   
+=================================================================
+embedding (Embedding)        (None, None, 32)          2834688   
+_________________________________________________________________
+lstm (LSTM)                  (None, 32)                8320      
+_________________________________________________________________
+dense (Dense)                (None, 1)                 33        
+=================================================================
+Total params: 2,843,041
+Trainable params: 2,843,041
+Non-trainable params: 0
+_________________________________________________________________
+'''
+
+#Training
+model.compile(loss = "binary_crossentropy", optimizer="rmsprop", metrics=['acc'])
+history = model.fit(train_data, train_label, epochs=10, validation_split=0.2)
+
+results = model.evaluate(test_data, test_labels)
+print(results)
