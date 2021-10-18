4286184380da316d66ac0f1e18e4d3f67dcff6b9
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-18 16:50:53 -0700

    Update RNNPlayGenerator.py

--- TensorFlow/RNNPlayGenerator.py
+++ TensorFlow/RNNPlayGenerator.py
@@ -3,6 +3,7 @@ from keras.preprocessing import sequence
 import keras
 import tensorflow as tf
 import numpy as np
+import os
 
 path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt') #Get file and save as shakespeare.txt
 
@@ -186,3 +187,67 @@ def loss(labels, logits):
 
 #Compile Model
 model.compile(optimizer = 'adam', loss=loss)
+
+#Creating Checkpoints
+#Directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+#Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath = checkpoint_prefix,
+    save_weights_only=True
+)
+
+#Training
+history = model.fit(data, epochs = 40, callbacks = [checkpoint_callback])
+
+#Loading the Model
+model = build_model(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, batch_size=1)
+
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+
+checkpoint_num = 10
+model.load_weights(tf.train.load_checkpoint("./training_checkpoints/ckpt_" + str(checkpoint_num)))
+model.build(tf.TensorShape([1, None]))
+
+#Generating Text
+def generate_text(model, start_string):
+    #Evaluation Step (generating text using the learned model
+
+    #Number of characters to generate
+    num_generate = 800
+
+    #Converting our start string  to numbers (vectorizing)
+    input_eval = [char2idx[s] for s in start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
+
+    #Empty string to store our results
+    text_generated = []
+
+    #Low temperatures results in more predictable text.
+    # Higher temperatures results in more surprising text
+    #Experiment to find the best setting
+    temperature = 1.0
+
+    #Here batch size == 1
+    model.reset_states()
+    for i in range(num_generate):
+        predictions = model(input_eval)
+        #remove the batch dimension
+        predictions = tf.squeeze(predictions, 0)
+
+        #using a categorical distribution to predict the character returned by the model
+        predictions = predictions / temperature
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
+
+        #We pass the predicted character as the next input to the model
+        #along with the previous hidden state
+        input_eval = tf.expand_dims([predicted_id], 0)
+
+        text_generated.append(idx2char[predicted_id])
+    return (start_string + ''.join(text_generated))
+
+inp = input("Type a starting string: ")
+print(generate_text(model, inp))
