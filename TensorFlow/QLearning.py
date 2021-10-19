691e53ac3ff54c24527084783f7d9c7929a6dcb5
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-19 14:32:37 -0700

    Update QLearning.py

--- TensorFlow/QLearning.py
+++ TensorFlow/QLearning.py
@@ -8,4 +8,23 @@ Instead of inputting tons of data, let the agent to learn. i.e. Learning how to 
 Goal is, 'Agent' explore the 'Environment' and go through some 'States' by 'Action' to achieve maximum 'Reward'
 
 Q-Learning : Creating table of 'Actions' and 'States', predicting the rewards of each state by the action
+
+Learning the Q-Table
+1. Randomly picking a valid action
+2. Using the current Q Table to find the best action
+We need both actions to be balanced properly. Otherwise, the agent will always try to maximize its reward for the current state/action,
+leading to 'local maxima'
+
+
+Updating Q Value
+Q[state, action] = Q[state, action] + A*(reward + r*max(Q[newState, :]) - Q[state, action])
+a : stands fo 'learning rate'
+r : stands for the 'discount factor'
+
+Learning Rate : Numeric constant that defines how much change is permitted on each QTable update.
+High learning rate means that each update will introduce a large change to the current state-action value.
+Small learning rate means that each update has a more subtle change
+
+Discount Factor : Balance how much focus is put on the current and future reward. High discount factor means that future rewards will be considered more heavily
+
 '''
