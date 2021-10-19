065fdece466ab289dd6dea03176804020bec7b11
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-18 17:07:14 -0700

    Create QLearning.py

---
+++ TensorFlow/QLearning.py
@@ -0,0 +1,11 @@
+'''Reinforcement Learning
+Instead of inputting tons of data, let the agent to learn. i.e. Learning how to play a game by actually playing it
+1) Environment : This is what our agent will explore. i.e. Level of the game, Or maze
+2) Agent : Entiry that's exploring the environment. i.e. Mario is agent in Mario game
+3) State : Where you are in the environment. i.e. position of agent is at in the environment
+4) Action : i.e. Moving to the left or right no did nothing
+5) Reward : What agent is trying to maximize
+Goal is, 'Agent' explore the 'Environment' and go through some 'States' by 'Action' to achieve maximum 'Reward'
+
+Q-Learning : Creating table of 'Actions' and 'States', predicting the rewards of each state by the action
+'''
